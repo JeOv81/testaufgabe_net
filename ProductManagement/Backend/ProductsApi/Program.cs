9b08b91f533ca3bfec086236618e2cf7687bc0fd
JeOv81 <JeOv81@gmail.com>
2025-07-23 16:20:26 +0200

    Scrutor: Add all handler to DI-Container
    - Added Scrutor for adding services and classes automatically

--- ProductManagement/Backend/ProductsApi/Program.cs
+++ ProductManagement/Backend/ProductsApi/Program.cs
@@ -4,6 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+// Scrutor Handler registrieren
+builder.Services.Scan(scan => scan
+    .FromAssemblies(typeof(Application.Features.Products.Commands.CreateProductCommandHandler).Assembly)
+    .AddClasses(classes => classes
+        .InNamespaces("Application.Features")
+        .Where(type =>
+            type.Name.EndsWith("Handler") &&
+            !type.IsAbstract && !type.IsGenericTypeDefinition))
+    .AsImplementedInterfaces()
+    .WithScopedLifetime());
+
 // Postgresql
 builder.AddNpgsqlDbContext<ProductsContext>(connectionName: "products-db");
 
