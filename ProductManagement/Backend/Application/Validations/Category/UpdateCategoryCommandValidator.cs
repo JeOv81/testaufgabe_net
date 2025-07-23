57c04adbbed957d95a015794faa444d611c77f80
J <jensov@gmx.de>
2025-07-23 17:55:40 +0200

    Added Validators

---
+++ ProductManagement/Backend/Application/Validations/Category/UpdateCategoryCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.Categories.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validations.Category;
+
+public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
+{
+    public UpdateCategoryCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Category ID is required for update.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Category name is required.")
+            .Length(3, 50).WithMessage("Category name must be between 3 and 50 characters.")
+            .Matches(@"^[a-zA-Z0-9]+$").WithMessage("Category name can only contain alphanumeric characters (no spaces or special characters).");
+    }
+}
---
+++ ProductManagement/Backend/Application/Validations/Products/GetProductByIdQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Products.Queries;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validations.Products;
+
+public class GetProductByIdQueryValidator : AbstractValidator<GetProductByIdQuery>
+{
+    public GetProductByIdQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Product ID is required.")
+            .Must(id => id != Guid.Empty).WithMessage("Product ID cannot be an empty GUID.");
+    }
+}
