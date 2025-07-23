309ebe479ee17c362f6816463d8495e6edd9fabc
JeOv81 <JeOv81@gmail.com>
2025-07-23 14:58:10 +0200

    Load entity configurations from Assembly

--- ProductManagement/Backend/Infrastructure/Persistence/ProductsContext.cs
+++ ProductManagement/Backend/Infrastructure/Persistence/ProductsContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,5 +20,7 @@ public class ProductsContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
