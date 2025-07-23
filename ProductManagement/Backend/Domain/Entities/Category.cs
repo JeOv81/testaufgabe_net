2e9378cafe7c34eb907329e975ef6114f6d79b61
JeOv81 <JeOv81@gmail.com>
2025-07-23 14:55:19 +0200

    Adding initial entities: Category and Product to the DbContext
    - The Id is a Guid

---
+++ ProductManagement/Backend/Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Category : IGuidEntity
+{
+    public Guid Id { get; set; }
+}
