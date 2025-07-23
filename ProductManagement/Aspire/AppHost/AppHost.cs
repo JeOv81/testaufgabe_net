7c5749ecb1aba8b74eae62a875b0878a075038b1
JeOv81 <JeOv81@gmail.com>
2025-07-23 14:28:22 +0200

    PostgreSql: added server and database

--- ProductManagement/Aspire/AppHost/AppHost.cs
+++ ProductManagement/Aspire/AppHost/AppHost.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sqlserver = builder.AddPostgres("postgresql")
+                                                       .WithLifetime(ContainerLifetime.Persistent);
+var productsDb = sqlserver.AddDatabase("products-db");
+
 builder.AddProject<Projects.ProductsApi>("products-api");
 
 builder.Build().Run();
