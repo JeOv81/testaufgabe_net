279ec14912aa7a56dcaf24f90f1ba4b2cda9df91
JeOv81 <JeOv81@gmail.com>
2025-07-23 14:55:19 +0200

    Adding initial entities: Category and Product to the DbContext
    - The Id is a Guid

--- ProductManagement/Backend/Infrastructure/Persistence/ProductsContext.cs
+++ ProductManagement/Backend/Infrastructure/Persistence/ProductsContext.cs
@@ -13,6 +13,8 @@ public class ProductsContext : DbContext
     {
     }
 
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
