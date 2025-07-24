e4414ae95627671514273c244ba868ad93f96d65
JeOv81 <JeOv81@gmail.com>
2025-07-25 00:16:43 +0200

    Added Missing Validation; Changed endpoints for validation

---
+++ ProductManagement/Backend/Application/Validations/Category/DeleteCategoryCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.Categories.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validations.Category;
+public class DeleteCategoryCommandValidator : AbstractValidator<DeleteCategoryCommand>
+{
+    public DeleteCategoryCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Category ID must not be empty.");
+    }
+}
